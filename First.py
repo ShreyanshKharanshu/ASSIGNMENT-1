c11ce7b05bb73e59f18dcbe4c7623ea717cd6008
Shreyansh Kharanshu <shreyaki.business@gmail.com>
2025-08-15 19:58:12 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,21 @@
+#Entering Numbers
+a=input('Enter your first number')
+b=input('Enter your second number')
+
+a=float(a)
+b=float(b)
+
+#calculating
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+
+#Result
+print("Additional:",c)
+
+print("Subtraction:", d)
+
+print("Multiplication:", e)
+
+print("Devide:", f)
---
+++ Secod.py
@@ -0,0 +1,6 @@
+#Input
+a=input('Enter your first name')
+b=input('Enter your last name')
+
+#Output
+print("Hello,",a,b,"Welcome to Python program")
